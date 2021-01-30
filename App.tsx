60da637a00fbf8712bdd06f55f68f2ba7ee46c51
markus douglas nascimento siqueira <makusdouglas@gmail.com>
2021-01-30 01:00:10 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+      <SafeAreaView>
+
+      <ScrollView bounces={true} horizontal >
+      
+      <LinearGradient 
+      style={styles.container} 
+      colors={['#ff3460','#ff3460', 'rgba(0,0,0,0.8)']}
+      start={{x: 0, y: 0}}
+      >
+      <StatusBar style="light" backgroundColor='#ff3460'/>
+
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Image source={require('./assets/adaptive-icon.png')}/>
+      </LinearGradient>
+      </ScrollView>
+      </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#ff3460',
     alignItems: 'center',
     justifyContent: 'center',
   },
